723cae53f10c3bf64b02b5835c7b84fbb63adc00
lloydvue <lloydvue94@gmail.com>
2022-04-10 16:04:58 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(driverArray, name) {
+    return driverArray.filter(driverName => driverName.toLowerCase() === name.toLowerCase());
+}
+//Check if drivers name is the name  for our filter feature
+
+function fuzzyMatch(driverArray, name) {
+    return driverArray.filter(person => person[0].toLowerCase() === name[0].toLowerCase());
+}
+//Match first letter to access via index of string
+function matchName(driverArray, name) {
+    return driverArray.filter(personObject => personObject.name === name);
+}
+//Specify person as an object
